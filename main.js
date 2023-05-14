9435619c393395eda65e4705c6b103a960ebd051
Eum SeungJu <sjsjmine1@naver.com>
2023-05-14 10:12:25 +0900

    learn

--- main.js
+++ main.js
@@ -12,75 +12,87 @@ var app = http.createServer(function(request,response){
 
   if(pathname === '/'){
     if(queryData.id === undefined ){
-      title = 'Welcome';
-      description = 'Hello, Node js';
-      var template = `
-      <!DOCTYPE html>
-    <html>
-    <head>  <!-- 문서의 정보들-->
-        <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
-        <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
-    
-        <link rel="stylesheet" href="style.css">
-        <script src="color.js"></script>
-    </head>
-    
-    <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
-        <h1><a href="/">WEB</a></h1>
-        <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
-    
-        <div id="grid">
-        <ol> <!-- Orderd list-->
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-    
-        <div>
-        <h2>${title}</h2> 
-        ${description}
-    </body>
-    </html>
-    `;
+
+      fs.readdir('./data',function(err, filelist){
+        title = 'Welcome';
+        description = 'Hello, Node js';
+
+        var list = '<ol>';
+        for(var i=0; i<filelist.length; i++){
+          list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`;
+        }
+        list = list +'</ol>';
+
+        var template = `
+        <!DOCTYPE html>
+      <html>
+      <head>  <!-- 문서의 정보들-->
+          <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
+          <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
+      
+          <link rel="stylesheet" href="style.css">
+          <script src="color.js"></script>
+      </head>
+      
+      <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
+          <h1><a href="/">WEB</a></h1>
+          <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
+      
+          <div id="grid">
+          ${list}
+      
+          <div>
+          <h2>${title}</h2> 
+          ${description}
+      </body>
+      </html>
+      `;
 
       response.writeHead(200);
       response.end(template);
+      });
     }
     else{
-      fs.readFile(`data/${queryData.id}`,'utf8', function(err,data){
-      description = data;
-      var template = `
-      <!DOCTYPE html>
-    <html>
-    <head>  <!-- 문서의 정보들-->
-        <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
-        <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
-    
-        <link rel="stylesheet" href="style.css">
-        <script src="color.js"></script>
-    </head>
-    
-    <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
-        <h1><a href="/">WEB</a></h1>
-        <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
-    
-        <div id="grid">
-        <ol> <!-- Orderd list-->
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-    
-        <div>
-        <h2>${title}</h2> 
-        ${description}
-    </body>
-    </html>
-    `;
-        response.writeHead(200);
-        response.end(template);
+      fs.readFile(`data/${queryData.id}`,'utf8', function(err,data){ 
+        fs.readdir('./data',function(err, filelist){
+          description = data;
+
+          var list = '<ol>';
+          for(var i=0; i<filelist.length; i++){
+            list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`;
+          }
+          list = list +'</ol>';
+
+          var template = `
+          <!DOCTYPE html>
+        <html>
+        <head>  <!-- 문서의 정보들-->
+            <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
+            <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
+        
+            <link rel="stylesheet" href="style.css">
+            <script src="color.js"></script>
+        </head>
+        
+        <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
+            <h1><a href="/">WEB</a></h1>
+            <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
+        
+            <div id="grid">
+            ${list}
+        
+            <div>
+            <h2>${title}</h2> 
+            ${description}
+        </body>
+        </html>
+        `;
+
+          response.writeHead(200);
+          response.end(template);
+        });
       });
       
     }
