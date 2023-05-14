481fec36c5475a63d9730e1e8a1b421631cc929a
Eum SeungJu <sjsjmine1@naver.com>
2023-05-14 23:09:57 +0900

    function

--- main.js
+++ main.js
@@ -2,6 +2,51 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); //url 정보들 가져와저장
 
+function templateHTML(title, list, body){
+  return `
+  <!DOCTYPE html>
+<html>
+<head>  <!-- 문서의 정보들-->
+    <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
+    <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
+    <style>
+      .grid-container {
+        display: grid;
+        grid-template-columns: 200px minmax(0, 1fr) minmax(0, 1fr); /* First column fixed, other columns adjust */
+        grid-gap: 10px
+      }
+    </style>
+</head>
+
+<body style="background-color: gray;color:powderblue;">  <!-- 문서의 내용물-->
+  <h1><a href="/">WEB</a></h1>
+  <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
+
+  <div class="grid-container">
+    <div>
+      ${list}
+    </div>
+    <div>
+      ${body}
+    </div>
+  </div>
+</body>
+</html>
+`;
+}
+
+function make_list(filelist){
+  var list = '<ol>';
+  for(var i=0; i<filelist.length; i++){
+    list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list = list +'</ol>';
+
+  return list;
+}
+
+
+
 var app = http.createServer(function(request,response){
   var _url = request.url; //  /? 뒤쪽 정보 url로부터 가져옴
   var queryData = url.parse(_url, true).query;
@@ -17,37 +62,9 @@ var app = http.createServer(function(request,response){
         title = 'Welcome';
         description = 'Hello, Node js';
 
-        var list = '<ol>';
-        for(var i=0; i<filelist.length; i++){
-          list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`;
-        }
-        list = list +'</ol>';
+        var list = make_list(filelist);
 
-        var template = `
-        <!DOCTYPE html>
-      <html>
-      <head>  <!-- 문서의 정보들-->
-          <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
-          <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
-      
-          <link rel="stylesheet" href="style.css">
-          <script src="color.js"></script>
-      </head>
-      
-      <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
-          <h1><a href="/">WEB</a></h1>
-          <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
-      
-          <div id="grid">
-          ${list}
-      
-          <div>
-          <h2>${title}</h2> 
-          ${description}
-      </body>
-      </html>
-      `;
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
 
       response.writeHead(200);
       response.end(template);
@@ -58,37 +75,9 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data',function(err, filelist){
           description = data;
 
-          var list = '<ol>';
-          for(var i=0; i<filelist.length; i++){
-            list = list +`<li><a href="/?id=${filelist[i]}">${filelist[i]}</li>`;
-          }
-          list = list +'</ol>';
+          var list = make_list(filelist);
 
-          var template = `
-          <!DOCTYPE html>
-        <html>
-        <head>  <!-- 문서의 정보들-->
-            <title>WEB1 - ${title}</title>  <!-- 페이지 제목-->
-            <meta charset="utf-8">   <!-- utf-8로 브라우저를 읽어라-->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> <!-- jQuery-->
-        
-            <link rel="stylesheet" href="style.css">
-            <script src="color.js"></script>
-        </head>
-        
-        <body style="background-color: black;color:white;">  <!-- 문서의 내용물-->
-            <h1><a href="/">WEB</a></h1>
-            <input id="night_day" type="button" value="[DAY]" onclick="nightDayHandler(this)">
-        
-            <div id="grid">
-            ${list}
-        
-            <div>
-            <h2>${title}</h2> 
-            ${description}
-        </body>
-        </html>
-        `;
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
 
           response.writeHead(200);
           response.end(template);
@@ -107,8 +96,5 @@ var app = http.createServer(function(request,response){
     response.end('Not found'); 
   }
   
-  
-  
-
 });
 app.listen(3000);
